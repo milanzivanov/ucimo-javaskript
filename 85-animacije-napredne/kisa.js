04e2845451001c5efa12b5295f819d5683d60094
Damjan Pavlica <mudroljub@gmail.com>
2017-03-02 00:49:00 +0100

    cistka

--- 85-animacije-napredne/kisa.js
+++ 85-animacije-napredne/kisa.js
@@ -31,7 +31,6 @@ const controls = {
   speed: 2
 }
 
-
 // LISTENERS
 
 window.onresize = function() {
@@ -44,25 +43,10 @@ window.onmousemove = function(event) {
   mouse.Y = event.clientY
 }
 
-
-// IZVRŠENJE
-
-window.onresize();
-
-(function boucle() {
-  requestAnimationFrame(boucle)
-  update()
-  rendu(ctx)
-})()
-
-
-
 // FUNKCIJE
 
-function Rain(X, Y, nombre) {
-  if (!nombre) {
-    nombre = nombreb
-  }
+function rain(X, Y) {
+  let nombre = nombreb
   while (nombre--) {
     particules.push({
       vitesseX: (Math.random() * 0.25),
@@ -73,12 +57,10 @@ function Rain(X, Y, nombre) {
       couleur: 'hsla(' + controls.color + ',' + controls.saturation + '%, ' + controls.lightness + '%,' + controls.opacity + ')',
     })
   }
-} // Rain
+}
 
-function explosion(X, Y, couleur, nombre) {
-  if (!nombre) {
-    nombre = nombrebase
-  }
+function explosion(X, Y, couleur) {
+  let nombre = nombrebase
   while (nombre--) {
     gouttes.push({
       vitesseX: (Math.random() * 4 - 2),
@@ -90,11 +72,9 @@ function explosion(X, Y, couleur, nombre) {
       couleur
     })
   }
-} // explosion
-
+}
 
 function rendu(ctx) {
-
   if (controls.multi == true) {
     controls.color = Math.random() * 360
   }
@@ -107,18 +87,15 @@ function rendu(ctx) {
   const goutteslocales = gouttes
   const tau = Math.PI * 2
 
-  for (var i = 0, particulesactives; particulesactives = particuleslocales[i]; i++) {
-
+  for (let i = 0, particulesactives; particulesactives = particuleslocales[i]; i++) {
     ctx.globalAlpha = particulesactives.alpha
     ctx.fillStyle = particulesactives.couleur
     ctx.fillRect(particulesactives.X, particulesactives.Y, particulesactives.vitesseY / 4, particulesactives.vitesseY)
   }
 
-  for (var i = 0, gouttesactives; gouttesactives = goutteslocales[i]; i++) {
-
+  for (let i = 0, gouttesactives; gouttesactives = goutteslocales[i]; i++) {
     ctx.globalAlpha = gouttesactives.alpha
     ctx.fillStyle = gouttesactives.couleur
-
     ctx.beginPath()
     ctx.arc(gouttesactives.X, gouttesactives.Y, gouttesactives.radius, 0, tau)
     ctx.fill()
@@ -157,13 +134,11 @@ function rendu(ctx) {
   }
 } // rendu
 
-
 function update() {
-
   const particuleslocales = particules
   const goutteslocales = gouttes
 
-  for (var i = 0, particulesactives; particulesactives = particuleslocales[i]; i++) {
+  for (let i = 0, particulesactives; particulesactives = particuleslocales[i]; i++) {
     particulesactives.X += particulesactives.vitesseX
     particulesactives.Y += particulesactives.vitesseY + 5
     if (particulesactives.Y > height - 15) {
@@ -191,7 +166,7 @@ function update() {
     }
   }
 
-  for (var i = 0, gouttesactives; gouttesactives = goutteslocales[i]; i++) {
+  for (let i = 0, gouttesactives; gouttesactives = goutteslocales[i]; i++) {
     gouttesactives.X += gouttesactives.vitesseX
     gouttesactives.Y += gouttesactives.vitesseY
     gouttesactives.radius -= 0.075
@@ -205,8 +180,18 @@ function update() {
     }
   }
 
-  var i = controls.rain
+  let i = controls.rain
   while (i--) {
-    Rain(Math.floor((Math.random() * width)), -15)
+    rain(Math.floor((Math.random() * width)), -15)
   }
-} // update
+}
+
+// INIT
+
+window.onresize()
+
+;(function boucle() {
+  requestAnimationFrame(boucle)
+  update()
+  rendu(ctx)
+})()
